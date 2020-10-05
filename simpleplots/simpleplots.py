609833f74627cadc0ad30592cd807ac6db9423d2
Alberto Burgos <albertoburgosplaza@gmail.com>
2020-10-05 18:34:04 +0000

    More types for dispatcher, histogram and boxplot

--- simpleplots/simpleplots.py
+++ simpleplots/simpleplots.py
@@ -1,12 +1,24 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from pandas.api.types import is_object_dtype
-from simpleplots.univariate import plot_object
+from pandas.api.types import (
+    is_object_dtype,
+    is_bool_dtype,
+    is_categorical_dtype,
+    is_numeric_dtype,
+)
+from simpleplots.univariate import plot_count, plot_hist, plot_box
 
 
 def plot(data: pd.DataFrame, attribute: [str, list], **kwargs):
     # Check if it's an univariate plot
     if isinstance(attribute, str):
         if is_object_dtype(data[attribute].dtype):
-            plot_object(data[attribute], **kwargs)
+            plot_count(data[attribute], **kwargs)
+        elif is_categorical_dtype(data[attribute].dtype):
+            plot_count(data[attribute], **kwargs)
+        elif is_bool_dtype(data[attribute].dtype):
+            plot_count(data[attribute], **kwargs)
+        elif is_numeric_dtype(data[attribute].dtype):
+            plot_hist(data[attribute], **kwargs)
     elif isinstance(attribute, list):
         print("Multivariate plot")
--- simpleplots/univariate.py
+++ simpleplots/univariate.py
@@ -3,9 +3,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_object(data: pd.Series, **kwargs):
+def plot_count(data: pd.Series, **kwargs):
     plt.figure(**kwargs)
     plt.title(f"{data.name} count")
     sns.countplot(x=data)
+    plt.xlabel(None)
+    plt.show()
+
+
+def plot_hist(data: pd.Series, **kwargs):
+    plt.figure(**kwargs)
+    plt.title(f"{data.name} histogram")
+    sns.histplot(x=data)
+    plt.xlabel(None)
+    plt.show()
+
+
+def plot_box(data: pd.Series, **kwargs):
+    plt.figure(**kwargs)
+    plt.title(f"{data.name} boxplot")
+    sns.boxplot(x=data)
     plt.xlabel(None)
     plt.show()
